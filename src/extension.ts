dd510d3263abe4a0e5386b34f5bbec7dbf8a5a60
Naman Sancheti <naman.sancheti@gmail.com>
2019-07-03 08:15:48 +0530

    Show information message on activation

--- src/extension.ts
+++ src/extension.ts
@@ -1,5 +1,11 @@
 import * as vscode from 'vscode';
 
-export function activate(context: vscode.ExtensionContext) {}
+export function activate(context: vscode.ExtensionContext) {
+	let disposable = vscode.commands.registerCommand('extension.askRxJSBuddy', () => {
+		vscode.window.showInformationMessage('RxJS buddy is activated!');
+	});
+
+	context.subscriptions.push(disposable);
+}
 
 export function deactivate() {}
